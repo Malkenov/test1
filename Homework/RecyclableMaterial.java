089ae6ae86577a3b98ed8725e81520a901899a9b
asanali_malkenov22@icloud.com <asanali_1997@mail.ru>
2024-12-06 19:13:39 +0500

    Двадцатая домашка

---
+++ Homework/RecyclableMaterial.java
@@ -0,0 +1,32 @@
+public class RecyclableMaterial {
+
+    // объявите переменные с правильными модификаторами
+    protected int code;
+    protected MaterialType type;
+    protected boolean isRecyclable;
+    protected String description;
+
+
+    RecyclableMaterial(int code, MaterialType type, boolean isRecyclable, String description) {
+        this.code = code;
+        this.type = type;
+        this.isRecyclable = isRecyclable;
+        this.description = description;
+    }
+
+    public int getCode() {
+        return code;
+    }
+
+    public MaterialType getType() {
+        return type;
+    }
+
+    public boolean isRecyclable() {
+        return isRecyclable;
+    }
+
+    public String getDescription() {
+        return description;
+    }
+}
